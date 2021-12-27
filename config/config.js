08fd0c59e4cad75a41d78bee5d4adb96534955ed
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2021-12-27 08:58:47 +0700

    init sequelize

---
+++ config/config.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const {
+  PG_HOST,
+  PG_USER,
+  PG_PASS,
+  PG_PORT,
+  PG_DB_NAME
+} = process.env
+
+module.exports = {
+  "development": {
+    "username": PG_USER,
+    "password": PG_PASS,
+    "database": PG_DB_NAME,
+    "host": PG_HOST,
+    "dialect": "postgres",
+    "port": PG_PORT
+  },
+  "test": {
+    "username": PG_USER,
+    "password": PG_PASS,
+    "database": PG_DB_NAME,
+    "host": PG_HOST,
+    "dialect": "postgres",
+    "port": PG_PORT
+  },
+  "production": {
+    "username": PG_USER,
+    "password": PG_PASS,
+    "database": PG_DB_NAME,
+    "host": PG_HOST,
+    "dialect": "postgres",
+    "port": PG_PORT,
+    "use_env_variable": 'DATABASE_URL',
+    "dialectOptions": {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    }
+  }
+}
