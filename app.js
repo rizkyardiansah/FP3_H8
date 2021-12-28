2186e2f5fd22f9199128cffcafcf231822b2ac6c
rizkyardiansah <muhammadrizkyardiansah93@gmail.com>
2021-12-28 10:23:23 +0700

    create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.listen(port, () => console.log(`Running on port ${port}`))
