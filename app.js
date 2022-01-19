7ebf0fb5fba8aaf8a24b24c1a4ff25398a117876
yolandari12 <yolandari.18411011@student.ubl.ac.id>
2022-01-19 06:29:01 -0800

    add route utama for user

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const userRoute = require('./routes/userRoute')
+const categoryRoute = require('./routes/categoryRoute')
 const productRoute = require('./routes/productRoute');
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/users/', userRoute)
+app.use('/categories/', categoryRoute)
 app.use('/products/', productRoute);
 
 app.listen(port, () => console.log(`Running on port ${port}`))
