7a42b63b5b755c82d02904b7cbe154cf6c71177d
yolandari12 <yolandari.18411011@student.ubl.ac.id>
2022-01-20 08:50:30 -0800

    Updates lagiiii

--- models/transaction.js
+++ models/transaction.js
@@ -1,107 +1,64 @@
 module.exports = (sequelize, DataTypes) => {
-    const Transaction = sequelize.define('Transaction', {
-        id: {
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-            type: DataTypes.INTEGER,
+  const Transactions = sequelize.define('Transactions', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    productId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    quantity: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      validate: {
+        notNull: {
+          msg: 'Quantity tidak boleh kosong'
         },
-        ProductId: {
-            allowNull: false,
-            type: DataTypes.INTEGER,
-        },
-        UserId: {
-            allowNull: false,
-            type: DataTypes.INTEGER,
-        },
-        quantity: {
-            allowNull: false,
-            type: DataTypes.INTEGER,
-            validate: {
-                notNull: {
-                    msg: "field 'quantity' is required"
-                },
-                isNumeric: {
-                    msg: "'quantity' value should be number"
-                }
-            }
-        },
-        total_price: {
-            allowNull: false,
-            type: DataTypes.INTEGER,
-            validate: {
-                notNull: {
-                    msg: "field 'total_price' is required"
-                },
-                isNumeric: {
-                    msg: "'total_price' value should be number"
-                }
-            }
-        },
-        createdAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
-        },
-        updatedAt: {
-            allowNull: false,
-            type: DataTypes.DATE,
+        isInt: {
+          args: true,
+          msg: 'Quantity hanya boleh bertipe integer'
         }
-    }, {
-        tableName: "TransactionHistory"
-    })
-
-    Transaction.associate = models => {
-        Transaction.belongsTo(models.User, {foreignKey: 'UserId'});
-        Transaction.belongsTo(models.Product, {foreignKey: 'ProductId'});
+      }
+    },
+    total_price: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      validate: {
+        notNull: {
+          msg: 'Harga total tidak boleh kosong'
+        },
+        isInt: {
+          args: true,
+          msg: 'Harga hanya boleh bertipe integer'
+        }
+      }
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
     }
-    Transaction.init({
-        ProductId: {
-          type: DataTypes.INTEGER,
-          validate: {
-            notEmpty: {
-              msg: "Kolom ini harus diisi!"
-            },
-            isInt: {
-              msg: "Tipe data harus Integer!"
-            }
-          }
-        },
-        UserId: {
-          type: DataTypes.INTEGER,
-          validate: {
-            notEmpty: {
-              msg: "Kolom ini harus diisi!"
-            },
-            isInt: {
-              msg: "Tipe data harus Integer!"
-            }
-          }
-        },
-        quantity: {
-          type: DataTypes.INTEGER,
-          validate: {
-            notEmpty: {
-              msg: "Kolom ini harus diisi!"
-            },
-            isInt: {
-              msg: "Tipe data harus Integer!"
-            }
-          }
-        },
-        total_price: {
-          type: DataTypes.INTEGER,
-          validate: {
-            notEmpty: {
-              msg: "Kolom ini harus diisi!"
-            },
-            isInt: {
-              msg: "Tipe data harus Integer!"
-            }
-          }
-        }
-      }, {
-        sequelize,
-        modelName: 'TransactionHistory',
-      });
-    return Transaction;
+  }, {
+    tableName: 'TransactionHistory'
+  })
+
+  Transactions.associate = models => {
+    Transactions.belongsTo(models.Product, {
+      foreignKey: 'ProductId'
+    })
+    Transactions.belongsTo(models.User, {
+      foreignKey: 'UserId'
+    })
+  }
+  return Transactions;
 }
