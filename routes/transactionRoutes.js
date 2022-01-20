efba295a3af6585699107c334beb109bae9f2f2c
yolandari12 <yolandari.18411011@student.ubl.ac.id>
2022-01-19 22:56:26 -0800

    edit router

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -3,8 +3,8 @@ const transactionController = require('../controllers/transactionController')
 const verifyToken = require('../middleware/verifyToken')
 const { authorizationTrans } = require('../middlewares/authTransaction')
 
-router.post('/transactions', verifyToken, transactionController.createTransaction);
-router.get('/transactions/user', verifyToken, transactionController.getTransUser);
-router.get('/transactions/admin', verifyToken, userAuthorization, transactionController.getTransAdmin);
-router.get('/transactions/:transactionId', verifyToken, authorizationTrans, transactionController.getTransId);
+router.post('/', verifyToken, transactionController.createTransaction);
+router.get('/user', verifyToken, transactionController.getTransUser);
+router.get('/admin', verifyToken, userAuthorization, transactionController.getTransAdmin);
+router.get('/:transactionId', verifyToken, authorizationTrans, transactionController.getTransId);
 module.exports = router
